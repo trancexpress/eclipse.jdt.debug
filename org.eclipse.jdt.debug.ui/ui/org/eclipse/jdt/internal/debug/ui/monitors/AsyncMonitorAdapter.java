6adf32c2279355d4e21c290bbd557418e7b27d03
Kevin Barnes <krbarnes>
2005-09-14 21:24:30 +0000

    converted DeferredWorkbenchAdapters to IPresentationAdapters

---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/monitors/AsyncMonitorAdapter.java
@@ -0,0 +1,29 @@
+package org.eclipse.jdt.internal.debug.ui.monitors;
+
+import org.eclipse.debug.internal.ui.elements.adapters.AbstractAsyncPresentationAdapter;
+import org.eclipse.jdt.internal.debug.ui.IJDIPreferencesConstants;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+
+public abstract class AsyncMonitorAdapter extends AbstractAsyncPresentationAdapter implements IPropertyChangeListener {
+
+	private boolean fDisplayMonitors;
+
+	public AsyncMonitorAdapter() {
+        IPreferenceStore preferenceStore = JDIDebugUIPlugin.getDefault().getPreferenceStore();
+		preferenceStore.addPropertyChangeListener(this);
+		fDisplayMonitors= preferenceStore.getBoolean(IJDIPreferencesConstants.PREF_SHOW_MONITOR_THREAD_INFO);
+	}
+
+	public void propertyChange(PropertyChangeEvent event) {
+		if (event.getProperty().equals(IJDIPreferencesConstants.PREF_SHOW_MONITOR_THREAD_INFO)) {
+			fDisplayMonitors= ((Boolean)event.getNewValue()).booleanValue();
+		}
+	}
+
+	protected boolean isDisplayMonitors() {
+	    return fDisplayMonitors;
+	}
+}
